0e421d3219ce17f48c5934c79b3978298f325d73
孔冬滨 <dbkong@leqee.com>
2021-12-03 10:59:17 +0800

    获取js真实类型

---
+++ 手写/获取js语言真实类型/index.js
@@ -0,0 +1,20 @@
+const trueTypeOf = obj => {
+  return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+};
+
+console.log(trueTypeOf(''));
+// string
+console.log(trueTypeOf(0));
+// number
+console.log(trueTypeOf());
+// undefined
+console.log(trueTypeOf(null));
+// null
+console.log(trueTypeOf({}));
+// object
+console.log(trueTypeOf([]));
+// array
+console.log(trueTypeOf(0));
+// number
+console.log(trueTypeOf(() => {}));
+// function
